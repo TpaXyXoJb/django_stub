0da58d371922e8f25cd9cacc1c526bcc25917c12
TpaXyXoJb <salnikovne@yandex.ru>
2024-11-30 22:45:31 +0700

    initial

---
+++ server/config/api_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from apps.pockets.routers import pockets_router
+
+urlpatterns = [
+    path('auth/', include('apps.users.urls.auth')),
+    path('users/', include('apps.users.urls.users_urls')),
+]
